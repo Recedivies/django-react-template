31e9ef3a3bcf570d8b1a1ccf6e3d90be6027a77d
=recedivies <ahmadhiprananta@gmail.com>
2022-07-08 09:26:00 +0700

    feat: added heroku configuration

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,10 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path(
         "api/",
         include("api.urls"),
     ),
+    re_path(".*", TemplateView.as_view(template_name="index.html")),
 ]
