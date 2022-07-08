8c815ef587c7692f5b01639aac55d7de49f098ea
=recedivies <ahmadhiprananta@gmail.com>
2022-07-08 13:21:45 +0700

    fix: code format

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from api.views import SimpleAPIView, HealthCheckAPIView
+from api.views import HealthCheckAPIView, SimpleAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
